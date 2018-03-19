e318b7dca7833e82fec650c626e77ec7b51a5cae
valentindo <valentindo.ci@gmail.com>
2018-03-19 14:19:21 +0100

    add function getUrlParams

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,25 @@ $(function(){
 	console.log('Ready');
 
 	//////////////////////////////////
+	//       Get URL Params        //
+	////////////////////////////////
+
+	// getUrlParameter: function(sParam) {
+	// 	var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+	// 		sURLVariables = sPageURL.split('&'),
+	// 		sParameterName,
+	// 		i;
+	//
+	// 	for (i = 0; i < sURLVariables.length; i++) {
+	// 		sParameterName = sURLVariables[i].split('=');
+	//
+	// 		if (sParameterName[0] === sParam) {
+	// 			return sParameterName[1] === undefined ? true : sParameterName[1];
+	// 		}
+	// 	}
+	// },
+
+	//////////////////////////////////
 	//   Call Ajax Api Instagram   //
 	////////////////////////////////
 
